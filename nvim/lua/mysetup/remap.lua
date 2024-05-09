1b73b048defd5e148f6de0ae47110551e01f8f23
aleksisiitari <aleksi.siitari@gmail.com>
2024-05-09 14:39:11 +0300

    Add remap from ; to ;

--- nvim/lua/mysetup/remap.lua
+++ nvim/lua/mysetup/remap.lua
@@ -4,6 +4,8 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n","<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
+vim.keymap.set("n", ";", ":")
+
 -- Preserve value in register when pasting
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
